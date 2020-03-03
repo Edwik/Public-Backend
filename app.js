3b178c707aec6b0fab135f903f2b93478c4514df
Edwin Anaya <eanaya@lean-tech.io>
2020-03-03 13:12:53 -0500

    no message

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 'use strict'
-/**
- * Date: 02/05/2020
- * Author: Edwin Anaya H.
- * Url: https://documentation.com/
- */
 
-const EXPRESS    = require('express')
-const BODYPARSER = require('body-parser')
-const APP        = EXPRESS()
-const ROUTES     = require('./routes')
-const CORS       = require('cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const api = require('./routes')
+const cors = require('cors')
 
-APP.use(BODYPARSER.urlencoded({ extended: false }))
-APP.use(BODYPARSER.json())
-APP.use(CORS())
-
-APP.use(function (req, res, next) {
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+    res.setHeader('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
-    next()
+    next();
 });
 
-APP.use('/api', ROUTES)
-
-APP.get('/', (req,res) =>{
-    res.status(200).send({msg: 'Hello Coder! Welcome to Api by Edwin Anaya.'})
+app.use('/api', api)
+app.get('/', (req,res) =>{
+    res.status(200).send({msg: 'Hello coder! welcome to Api by Edwin Anaya.'})
 })
 
-// APP.use(response);
-
-APP.use(function(req, res, next) {
-    return res.status(404).send({ msg: `Route ${req.url} Not Found.`})
+app.use(function(req, res, next) {
+    return res.status(404).send({ msg: 'Route'+req.url+' Not found.' });
 });
 
-module.exports = APP
+// app.use(function(err, req, res, next) {
+//     return res.status(500).send({ msg: "500 server error", error: err });
+// });
+
+module.exports = app
